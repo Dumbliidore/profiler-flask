ecb6269a6dd86941fb4ba370c07962ad5d44a009
Ayden <2361018131@qq.com>
2024-04-09 16:44:27 +0800

    🎉 init(*): Hello, profiler-flask

---
+++ example/app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+import flask_profiler
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config["profiler"] = {
+    "storage": {"engine": "sqlite"},
+    "basicAuth": {"enabled": True, "username": "admin", "password": "admin"},
+    "ignore": ["^/static/.*"],
+}
+
+
+@app.route("/product/<id>", methods=["GET"])
+def getProduct(id):
+    return "product id is " + str(id)
+
+
+@app.route("/product/<id>", methods=["PUT"])
+def updateProduct(id):
+    return "product {} is being updated".format(id)
+
+
+@app.route("/products", methods=["GET"])
+def listProducts():
+    return "suppose I send you product list..."
+
+
+flask_profiler.init_app(app)
+
+
+@app.route("/doSomething", methods=["GET"])
+def doSomething():
+    return "profiler will not measure this."
+
+
+@app.route("/doSomethingImportant/<id>", methods=["GET"])
+@flask_profiler.profile()
+def doSomethingImportant(id):
+    import time
+
+    time.sleep(4)
+    return "profiler will measure this request."
